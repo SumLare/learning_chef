c9ae1d3c25391723c63fc784eb0b6a0052608a2b
SumLare <mrhard95@gmail.com>
2016-11-09 11:21:13 +0000

    Add unit test for apache cookbook

--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -7,14 +7,19 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When platform is centos with version 7.2.1511' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'start and enable apache2' do
+      expect(chef_run).to start_service('apache2')
+      expect(chef_run).to enable_service('apache2')
     end
   end
 end
